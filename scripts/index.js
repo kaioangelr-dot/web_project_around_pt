a108d7f535ad16288a666b037d0c4f177a5a1e3e
KaioR <kaioangel.r@gmail.com>
2026-04-04 14:25:32 -0300

    feat: add profile edit popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,13 @@
+const editBtn = document.querySelector(".profile__edit-button");
+const editPopup = document.querySelector("#edit-popup");
+const editCloseBtn = editPopup.querySelector(".popup__close");
+
+const profileName = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+
+const editName = editPopup.querySelector(".popup__input_type_name");
+const editDescription = editPopup.querySelector(".popup__input_type_description"); /* prettier-ignore */
+
 let initialCards = [
   {
     name: "Vale de Yosemite",
@@ -25,6 +35,41 @@ let initialCards = [
   },
 ];
 
-for (i = 0; i < initialCards.length; i++) {
+for (let i = 0; i < initialCards.length; i++) {
   console.log(initialCards[i].name);
 }
+
+function openModal(modal) {
+  modal.classList.add("popup_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("popup_is-opened");
+}
+
+function fillProfileForm() {
+  editName.value = profileName.textContent;
+  editDescription.value = profileDescription.textContent;
+}
+
+function handleOpenEditModal() {
+  openModal(editPopup);
+  fillProfileForm();
+}
+
+editBtn.addEventListener("click", handleOpenEditModal);
+
+editCloseBtn.addEventListener("click", function () {
+  closeModal(editPopup);
+});
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  profileName.textContent = editName.value;
+  profileDescription.textContent = editDescription.value;
+
+  closeModal(editPopup);
+}
+
+editPopup.addEventListener("submit", handleProfileFormSubmit);
